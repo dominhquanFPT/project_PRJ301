7f547fd4e12188b27c3ffb63adcdc93c4229963e
quan <cchheellpani@gmail.com>
2023-10-20 17:22:20 +0700

    search product by name

--- FlowerShop/src/java/dao/ProductsDAO.java
+++ FlowerShop/src/java/dao/ProductsDAO.java
@@ -89,5 +89,33 @@ public class ProductsDAO extends MyDAO {
 
    
     }
+    
+      // hàm search san pham
+    public List<Products> searchProductByName(String text) {
+        List<Products> productList = new ArrayList<>();
+        xSql = "SELECT * FROM Products WHERE Name LIKE ?";
+        try {
+            int id, price, sellerID, categoryID;
+            String name, description, image;
+            Products product;
+            ps = con.prepareStatement(xSql);
+            ps.setString(1, "%" + text + "%");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                id = rs.getInt("ID");
+                name = rs.getString("Name");
+                description = rs.getString("Description");
+                price = rs.getInt("Price");
+                image = rs.getString("Image");
+                categoryID = rs.getInt("CategoryID");
+                product = new Products(id, name, description, price, image, categoryID);
+                productList.add(product);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return productList;
+    }
 
 }
