f8be317f57ad97e7a35765246d0430462cd4b3ba
quan <cchheellpani@gmail.com>
2023-10-27 16:54:36 +0700

    order detail
    view detail of the order

--- FlowerShop/src/java/dao/OrderItemsDAO.java
+++ FlowerShop/src/java/dao/OrderItemsDAO.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import model.*;
 
-
 public class OrderItemsDAO extends MyDAO {
-
 
     // thêm 1 orderItems vào order moi
     public void insert(int ProductID, int Quantity) {
@@ -22,5 +20,32 @@ public class OrderItemsDAO extends MyDAO {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    // lay ra thong tin cua don hàng
+    public List<OrderItems> getOdDetailByOdId(int odId) {
+        List<OrderItems> List = new ArrayList<>();
+        xSql = "select oi.*, p.Name, p.Price from OrderItems oi join Products p\n"
+                + "on oi.ProductID = p.ID where oi.OrderID = ?";
+        try {
+            int productId, quantity, price;
+            String name;
+            OrderItems detail;
+            ps = con.prepareStatement(xSql);
+            ps.setInt(1, odId);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                productId = rs.getInt("ProductID");
+                name = rs.getString("Name");
+                quantity = rs.getInt("Quantity");
+                price = rs.getInt("Price");
+                detail = new OrderItems(odId, productId, name, quantity, price);
+                List.add(detail);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return List;
     }
 }
