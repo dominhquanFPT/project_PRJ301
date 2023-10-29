1e9915f310e2bd3059026768f2ed857081317f70
quan <cchheellpani@gmail.com>
2023-10-29 16:07:48 +0700

    management category
    CRUD

--- FlowerShop/src/java/dao/CategoriesDAO.java
+++ FlowerShop/src/java/dao/CategoriesDAO.java
@@ -3,7 +3,6 @@ package dao;
 import java.util.*;
 import model.*;
 
-
 public class CategoriesDAO extends MyDAO {
 
     // lay ra tat ca category
@@ -28,5 +27,49 @@ public class CategoriesDAO extends MyDAO {
 
         return categoriesList;
     }
+    // thêm moi 1 category
 
+    public void addCategory(String categoryName) {
+        xSql = "INSERT INTO Categories (Name) VALUES (?)";
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setString(1, categoryName);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // cap nhat category
+    public void updateCategory(int cateID, String newName) {
+        xSql = "UPDATE Categories SET Name = ? WHERE ID = ?";
+        try {
+            ps = con.prepareStatement(xSql);
+            ps.setString(1, newName);
+            ps.setInt(2, cateID);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // xóa category
+    public void deleteCategory(int cateID) {
+        try {
+            // Set foreign key references to null in other tables
+            xSql = "UPDATE Products SET CategoryID = NULL WHERE CategoryID = ?";
+            ps = con.prepareStatement(xSql);
+            ps.setInt(1, cateID);
+            ps.executeUpdate();
+
+            // Delete the category from Categories table
+            xSql = "DELETE FROM Categories WHERE ID = ?";
+            ps = con.prepareStatement(xSql);
+            ps.setInt(1, cateID);
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
